17e52c77d91c3f031d120a477864a609ee0cc6a1
atique-atq <atique.atq@gmail.com>
2022-08-22 23:20:14 +0600

    handling maximum five item

--- js/index.js
+++ js/index.js
@@ -1,15 +1,23 @@
+// getting the player name 
 document.getElementById('players-container').addEventListener('click', function (event) {
-    let btn = document.getElementById(event.target.id);
-    btn.setAttribute('disabled', true);
-    let playerName = event.target.previousElementSibling.firstChild.nextElementSibling.innerText;
-    displayPlayerList(playerName)
+    // if five name is added already, it will only give error alert
+    if (document.querySelectorAll('ol li').length === 5) { 
+        alert("Sorry!! You Can Not Pick more than five players!!")
+    }
+    else {
+        let btn = document.getElementById(event.target.id);
+        btn.setAttribute('disabled', true);
+        // accessing player name 
+        let playerName = event.target.previousElementSibling.firstChild.nextElementSibling.innerText;
+        displayPlayerList(playerName)
+    }
 })
 
-function displayPlayerList(playerName) {
+//displaying player name
+function displayPlayerList(playerName) {    
     let ol = document.getElementById('selected-player-list');
     let listItem = document.createElement('li');
     listItem.classList.add("list-group-item", "text-light", "p-1");
     listItem.innerText = playerName;
-    console.log(listItem);
     ol.appendChild(listItem);
 }
