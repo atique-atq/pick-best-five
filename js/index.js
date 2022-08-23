91ec0b5efa0100b504de7eee285a8d7749b02f2b
atique-atq <atique.atq@gmail.com>
2022-08-23 12:16:33 +0600

    added player expenses

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // getting the player name 
 document.getElementById('players-container').addEventListener('click', function (event) {
     // if five name is added already, it will only give error alert
-    if (document.querySelectorAll('ol li').length === 5) { 
+    if (getPlayerNumber() === 5) { 
         alert("Sorry!! You Can Not Pick more than five players!!")
     }
     else {
@@ -20,4 +20,24 @@ function displayPlayerList(playerName) {
     listItem.classList.add("list-group-item", "text-light", "p-1");
     listItem.innerText = playerName;
     ol.appendChild(listItem);
+}
+
+//get current selected players
+function getPlayerNumber() {
+    return (document.querySelectorAll('ol li').length)
+}
+
+//calculate total player price
+function calculatePlayerPrice() {
+    let inputPlayerPriceField = document.getElementById('perPlayerCostInput')
+    let inputPlayerPriceInNumber = parseFloat(inputPlayerPriceField.value) 
+    if (inputPlayerPriceInNumber > 0) {
+        totalPlayerPrice = getPlayerNumber() * inputPlayerPriceInNumber
+        document.getElementById('player-total-expense').innerText = totalPlayerPrice;
+        makeEmptyInputValue(inputPlayerPriceField)
+    } 
+}
+
+function makeEmptyInputValue(inputField) { 
+    inputField.value = '';
 }
